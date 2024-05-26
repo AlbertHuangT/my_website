dc880ed4ed3005da8df42a21657850acf5408d4f
AlbertHuangT <143773942+AlbertHuangT@users.noreply.github.com>
2024-05-26 14:03:17 -0700

    优化

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('Document fully loaded and parsed');
 
+    function createRipple(event) {
+        const button = event.currentTarget;
+        const ripple = document.createElement('span');
+        ripple.classList.add('ripple');
+        const rect = button.getBoundingClientRect();
+        const clientX = event.clientX || event.touches[0].clientX;
+        const clientY = event.clientY || event.touches[0].clientY;
+        ripple.style.left = `${clientX - rect.left}px`;
+        ripple.style.top = `${clientY - rect.top}px`;
+        button.appendChild(ripple);
+
+        setTimeout(() => {
+            ripple.remove();
+        }, 300); // 300ms 后移除波浪效果
+    }
+
     document.querySelectorAll('button').forEach(button => {
         button.addEventListener('click', function (e) {
             e.preventDefault(); // 阻止默认的按钮点击行为
-
-            const ripple = document.createElement('span');
-            ripple.classList.add('ripple');
-            const rect = this.getBoundingClientRect();
-            ripple.style.left = `${e.clientX - rect.left}px`;
-            ripple.style.top = `${e.clientY - rect.top}px`;
-            this.appendChild(ripple);
+            createRipple(e);
 
             // 延迟页面跳转
-            const href = this.getAttribute('onclick').match(/'([^']+)'/)[1];
+            const href = this.getAttribute('data-href');
             setTimeout(() => {
                 window.location.href = href;
             }, 300); // 300ms 的延迟
+        });
 
-            setTimeout(() => {
-                ripple.remove();
-            }, 300); // 300ms 后移除波浪效果
+        button.addEventListener('touchstart', function (e) {
+            createRipple(e);
         });
     });
 });
